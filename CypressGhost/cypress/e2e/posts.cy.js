0cf1f18c6c1f9b857b4fe22e21d1b18f85a581e5
Jesus Diaz <jd.diazd1@uniandes.edu.co>
2022-11-11 19:11:37 -0500

    test: Add posts test

---
+++ CypressGhost/cypress/e2e/posts.cy.js
@@ -0,0 +1,29 @@
+import {testLogin} from "../utils";
+
+describe('The posts page', () => {
+  it('successfully create', () => {
+    testLogin();
+
+    // we should be redirected to /dashboard
+    cy.visit('http://localhost:2368/ghost/#/posts')
+
+    cy.contains('New post').parent().click()
+
+    cy.url().should('contain', '/editor/post')
+
+    cy.get('textarea.gh-editor-title') // 2.
+        .type('My First Post') // 3.
+
+
+    cy.get('div.koenig-editor__editor') // 2.
+        .type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel interdum quam, vitae consequat sem. Vivamus malesuada dictum enim a porta. Ut ultrices ante sed nisl mollis, eu dapibus dui venenatis. Maecenas pretium purus non posuere elementum. Quisque iaculis, ex pharetra porttitor lacinia, velit est lobortis ligula, sit amet accumsan metus turpis ut risus.') // 3.
+
+    cy.contains('Publish').click()
+
+    cy.contains('Continue, final review →').click()
+
+    cy.contains('Publish post, right now').click()
+
+    cy.get('div.gh-post-bookmark-title').should('contain', 'My First Post')
+  })
+})
